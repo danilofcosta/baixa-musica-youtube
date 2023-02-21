0edcd249f911772d562648e6fcb85b5e8cc23904
Danilo Costa <117859128+danilofcosta@users.noreply.github.com>
2023-02-21 07:36:33 -0300

    otmizando o codigo e tirando info da tela

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # baixa-musica-youtube
 baixa musica e playliste de musicas do youtube 
+
+Este código irá produzir a seguinte saída:
+
+```console
+$ python3 script.py
+Hello World!
--- main.py
+++ main.py
@@ -1,52 +1,61 @@
+from pytube.cipher import chain
+
 from pytube import YouTube, Playlist
-import moviepy.editor as mp
-import re, os ,time
-from tqdm import tqdm as q
-
-def baixarmusic(path,link):   
-    print('INICIANDO ')
-    if 'playlist' in link:
-        print('INICIANDO Download DA Playlist')
-        yt=Playlist(link)
-        for music in q(yt):
-            k = YouTube(music)
-            ys = k.streams.filter(only_audio=True).first().download(path)
-    else:
-        yt = YouTube(link)
-        print(f'INICIANDO Download DA MUSICA {yt.title}')
-        for i in q(range(100)):
-            ys = yt.streams.filter(only_audio=True).first().download(path)
+import os,telebot
+from moviepy.editor import *
 
 
-    print('INICIANDO conversão da musica =>',yt.title)
-    for file in os.listdir(path):                 
-        if re.search('mp4', file):                                    
-            mp4_path = os.path.join(path , file)  
-            mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3') 
-            new_file = mp.AudioFileClip(mp4_path) 
-            new_file.write_audiofile(mp3_path)    
-            os.remove(mp4_path)                 
-    print("Download Completo")
+TOKEN = "tokem" 
+bot = telebot.TeleBot(TOKEN)
+user = bot.get_me()
+
+def sending(file,chatid):
+        bot.send_audio(chatid,audio = open(f"{file[:-4]+'.mp3'}", 'rb'),caption=':)',disable_notification=True)
 
 
-def pasta_out():
-    arquivo = '.\\music\\'
-    if os.path.exists(arquivo):
-        print('O caminho {} existe'.format(arquivo))
+def baixayt(link,id):
+    #verifica se o link e de playlist or lista de reprodução
+    try:
+        p=[Playlist(link)]
+        listLink=p[0]
+        if len(listLink)==0:
+            listLink.append(link)      
+    except:
+        listLink=[]
+        listLink.append(link) 
+
+    #verifica se a pasta em que  download caso n existir a cria
+    #if os.path.isfile(os.path.join(os.getcwd(),'music')):#msm tendo pasta e retornado false revisar //////////////////////////
+    ff=False
+    for f in os.listdir(os.getcwd()): 
+        if f=='music':
+            ff=True
+    if ff:
+        pass
     else:
         os.mkdir('music')
+    outdir=os.path.join(os.getcwd(),'music')
 
-pasta_out()
-path='.\\music\\'
+    #pesquisa e baixa a musica e tbm a converte para mp3 e remove mp4
+    for i in listLink:
+        yt=YouTube(i)
+        hd=yt.streams.filter(only_audio=True,abr='128kbps',).first().download(outdir)
+        fileyt =AudioFileClip(hd).write_audiofile(hd[:-4]+'.mp3',verbose=False,progress_bar=False)
+        fileyt =AudioFileClip(hd).close()
+        os.remove(hd)
+        sending(hd,id)
 
 
-while True:
-    f=str(input('Link da musica : '))
-    if f == '999':
-        print('saindo.........')
-        break
-    else:
-        baixarmusic(path,f)
-    os.system('msg * "MUSICAS BAIXADAS"')
-    time.sleep(5)
-    os.system('cls')
+
+@bot.message_handler(func=lambda messagem:True)
+def allmsg(m): 
+    link=m.text
+    if 'https:/' in link and 'yo' in link:
+        baixayt(link,int(m.chat.id))
+        try:
+            os.system('rm -r music/')#caso tiver usando link
+        except:
+            os.remove('os.path.join(os.getcwd(),'music')')
+
+print(f'rodandando no bot {user.first_name} .............')
+bot.polling()
